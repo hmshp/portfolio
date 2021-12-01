2a26702d8376d67bfcb5b715d61648d5d6e892a9
Hyemi Son <hyemisonne@gmail.com>
2021-12-01 16:17:08 +0900

    Change button elements' class to id

---
+++ new-version/ver.5/app-5.js
@@ -0,0 +1,40 @@
+"use strict";
+const home = document.querySelector("#home");
+const homeHeight = home.getBoundingClientRect().height;
+const VISIBLE_CLASSNAME = "visible";
+const arrowUp = document.querySelector("#arrow-up");
+const navbarToggleBtn = document.querySelector(".navbar-toggle-btn");
+const nav = document.querySelector(".navbar");
+const dayNightToggleIcon = document.querySelector(".day-night-toggle");
+
+/* arrow up 버튼 스크롤 Home 높이의 반 이상 내리면 화살표 아이콘 보이게 하는 작업 */
+document.addEventListener("scroll", () => {
+  if (window.scrollY > homeHeight / 2) {
+    arrowUp.classList.add(VISIBLE_CLASSNAME);
+  } else {
+    arrowUp.classList.remove(
+      VISIBLE_CLASSNAME
+    ); /* 스크롤 다시 위로 올리면 화살표 사라지게 */
+  }
+});
+
+/* 화살표 누르면 맨 위로 가게 하는 작업 */
+arrowUp.addEventListener("click", () => {
+  document.documentElement.scrollTop = 0;
+});
+
+/* 작은 화면에서 토글 버튼(navbar-toggle-btn) 누르면 메뉴 보이게 하는 작업 */
+navbarToggleBtn.addEventListener("click", () => {
+  nav.classList.toggle("visible");
+});
+
+/* day-night 모드 토글 기능(night 모드일 때는 배경색과 글자색 바꾸기) */
+dayNightToggleIcon.addEventListener("click", () => {
+  document.body.classList.toggle("dark-mode");
+  if (document.body.classList.contains("dark-mode")) {
+    dayNightToggleIcon.innerHTML = `<i class="fas fa-toggle-on"></i>`;
+  } else {
+    dayNightToggleIcon.innerHTML = `<i class="fas fa-toggle-off"></i>`;
+  }
+  /* 다크 모드일 때 클릭하면 on 아이콘으로 바꾸고 데이 모드일 때 클릭하면 off 아이콘으로 바꾸기 */
+});
